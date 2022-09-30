596a23780fe8a437082d051c65ffcd95c687df34
sigped12 <112166811+sigped12@users.noreply.github.com>
2022-09-30 12:09:30 +0200

    Started to get the game working
    Modified and added the divs for chips, money, shop and stats in the html and added variables and functions for chips, money and selling the chips in the js

--- game.js
+++ game.js
@@ -1,19 +1,43 @@
 var chips = 0;
 var chips_add = 1;
 var clickers = 0;
+var chips_produced = 0;
+var chips_sold = 0;
+var money = 0;
+var employees = 1;
+var factories = 1;
+var money_for_chips = 10;
 
 var chips_img = document.querySelector("#chips_img");
 var chips_amount = document.querySelector("#chips_amount");
+var money_amount = document.querySelector("#money_amount");
 
 chips_amount.innerHTML=chips;
 
+//function that runs when the chips is clicked
 function chips_click(){
-    chips + chips_add;
+    chips = chips + chips_add;
+    chips_produced = chips_produced + chips;
     chips_amount.innerHTML=chips;
+    console.log("+ " + chips_add + " chips");
 };
 
-var timer = setInterval(myTimer, 10);
-function myTimer(){
+//selling
+sell_amount = employees * 2;
+
+var sell_timer = setInterval(sellTimer, 1000);
+function sellTimer(){
+    if (chips > sell_amount) {
+        chips = chips - sell_amount;
+        chips_sold = chips_sold + sell_amount;
+        money = money_for_chips * sell_amount;
+        money_amount.innerHTML=money;
+    }
+};
+
+//clicker
+var clicker_timer = setInterval(clickerTimer, 10);
+function clickerTimer(){
     chips += clickers;
     chips_amount.innerHTML=chips;
 };
