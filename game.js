9749b55426e1322c61820121a6f9ebae02b5c337
sigped12 <112166811+sigped12@users.noreply.github.com>
2022-10-07 11:14:08 +0200

    Sell function update
    Added individual seller function for workers.

--- game.js
+++ game.js
@@ -12,81 +12,75 @@ var chips_img = document.querySelector("#chips_img");
 var chips_amount = document.querySelector("#chips_amount");
 var money_amount = document.querySelector("#money_amount");
 var clicker_img = document.querySelector("#clicker_img");
-var clicker_amount = document.querySelector("#clicker_amount");
+var clicker_values = document.querySelector("#clicker_values");
 var worker_img = document.querySelector("#worker_img");
-var worker_amount = document.querySelector("#worker_amount");
+var worker_values = document.querySelector("#worker_values");
 
-worker_amount.innerHTML=workers;
+// set
+var clicker_price = 40;
+var worker_price = 40;
+worker_values.innerHTML="Price: " + worker_price + "<br>Amount: " + workers;
+clicker_values.innerHTML="Price: " + clicker_price + "<br>Amount: " + clickers;
 
 // function that runs when the chips is clicked
 function chips_click(){
     chips = chips + chips_add;
     chips_produced = chips_produced + chips;
     console.log("+ " + chips_add + " chips");
-};
+}
 
 // update
 var update_timer = setInterval(updateTimer, 10);
 function updateTimer(){
     money_amount.innerHTML=money;
     chips_amount.innerHTML=chips;
-};
+}
 
 // selling
-
-/*
+var worker_sell_interval = 1000;
+var worker_sell_timer = setInterval(worker_sell, worker_sell_interval);
 function worker_sell(){
+    worker_sell_amount = workers * 2;
+    worker_money_add = money_for_chips * worker_sell_amount;
 
-};
-
-function clicker_sell(){
-
-};
-*/
-
-var sell_timer = setInterval(sellTimer, 1000);
-function sellTimer(){
-    sell_amount = workers * 2;
-    money_add = money_for_chips * sell_amount;
-
-    if (chips > sell_amount) {
-        chips -= sell_amount;
-        chips_sold += sell_amount;
-        money += money_add;
-        console.log("+ " + money_add + " money");
-    };
-};
+    if (chips > worker_sell_amount) {
+        chips -= worker_sell_amount;
+        chips_sold += worker_sell_amount;
+        money += worker_money_add;
+        console.log("+ " + worker_money_add + " money");
+    }
+}
 
 // clicker interval
 var clicker_interval = 10000;
 var clicker_timer = setInterval(clickerTimer, clicker_interval);
 
-var clicker_price = 40;
-
 function buy_clicker(){
     if (money >= clicker_price){
         clickers++;
-        clicker_amount.innerHTML=clickers;
         money -= clicker_price;
-    };
-};
+        clicker_interval -= 20;
+        clicker_price += 20
+        clicker_values.innerHTML="Price: " + clicker_price + "<br>Amount: " + clickers;
+    }
+}
 
 // clicker timer, repeats depending on interval
 function clickerTimer(){
     chips += clickers;
     chips_amount.innerHTML=chips;
-};
+}
 
 // worker
-var worker_price = 40;
 
 function buy_worker(){
     if (money >= worker_price){
         workers++;
-        worker_amount.innerHTML=workers;
         money -= worker_price;
-    };
-};
+        worker_price += 20
+        worker_values.innerHTML="Price: " + worker_price + "<br>Amount: " + workers;
+    }
+}
 
 chips_img.addEventListener("click", chips_click);
 clicker_img.addEventListener("click", buy_clicker);
